5aed77035db3260e58a50149efd5a443ad0d57ea
Nick Black <dank@qemfd.net>
2010-09-05 18:17:51 -0400

    basics

---
+++ Makefile
@@ -0,0 +1,9 @@
+.DELTE_ON_ERROR:
+.DEFAULT_GOAL:=test
+.PHONY: all test clean
+
+all:
+
+test: all
+
+clean:
---
+++ src/blossom.h
@@ -0,0 +1,12 @@
+#ifndef LIBBLOSSOM_BLOSSOM
+#define LIBBLOSSOM_BLOSSOM
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
