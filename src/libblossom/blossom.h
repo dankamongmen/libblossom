b345f469fad4a14a77cddb86bc04876d16c58fc0
Nick Black <dank@qemfd.net>
2010-09-13 08:22:13 -0400

    use non_null attribute where appropriate

--- src/libblossom/blossom.h
+++ src/libblossom/blossom.h
@@ -17,22 +17,28 @@ typedef struct blossom_state {
 } blossom_state;
 
 // Bloom tidcount threads. Any creation failure is a failure throughout.
-int blossom_pthreads(blossom_state *,const pthread_attr_t *,
-			void *(*)(void *),void *)
+int blossom_pthreads(blossom_state * __attribute__ ((non_null)),
+			const pthread_attr_t *,
+			void *(*)(void *) __attribute__ ((non_null)),
+			void *)
 	__attribute__ ((visibility ("default")))
 	__attribute__ ((warn_unused_result));
 
 // Bloom tidcount threads per processing element. Any creation failure is a
 // failure throughout.
-int blossom_per_pe(blossom_state *,const pthread_attr_t *,
-			void *(*)(void *),void *)
+int blossom_per_pe(blossom_state * __attribute__ ((non_null)),
+			const pthread_attr_t *,
+			void *(*)(void *) __attribute__ ((non_null)),
+			void *)
 	__attribute__ ((visibility ("default")))
 	__attribute__ ((warn_unused_result));
 
 // Bloom tidcount threads per processing element, binding them to the
 // appropriate processor. Any creation failure is a failure throughout.
-int blossom_on_pe(blossom_state *,const pthread_attr_t *,
-			void *(*)(void *),void *)
+int blossom_on_pe(blossom_state * __attribute__ ((non_null)),
+			const pthread_attr_t *,
+			void *(*)(void *) __attribute__ ((non_null)),
+			void *)
 	__attribute__ ((visibility ("default")))
 	__attribute__ ((warn_unused_result));
 
