df6a4a51d0cec9c773c4c6a476c9f27dce39b182
Nick Black <dank@qemfd.net>
2010-09-13 08:10:30 -0400

    move cpuset_count() into blossom.c

--- src/libblossom/blossom.c
+++ src/libblossom/blossom.c
@@ -1,7 +1,43 @@
 #include <errno.h>
 #include <stdlib.h>
+#include <pthread.h>
 #include <libblossom/blossom.h>
-#include <libblossom/schedule.h>
+
+#if defined(__linux__)
+#include <sched.h>
+#elif defined(__freebsd__)
+#include <sys/param.h>
+#include <sys/cpuset.h>
+typedef cpuset_t cpu_set_t;
+#else
+#error "Need cpu_set_t definition for this platform" // solaris is psetid_t
+#endif
+
+// FreeBSD's cpuset.h (as of 7.2) doesn't provide CPU_COUNT, nor do older
+// Linux setups (including RHEL5). This requires only CPU_{SETSIZE,ISSET}.
+static inline unsigned
+portable_cpuset_count(void){
+        unsigned count = 0,cpu;
+	cpu_set_t mask;
+
+#if defined(__freebsd__)
+        if(cpuset_getaffinity(CPU_LEVEL_CPUSET,CPU_WHICH_CPUSET,-1,
+                                sizeof(mask),&mask) == 0){
+#elif defined(__linux__)
+        // We might be only a subthread of a larger application; use the
+        // affinity mask of the thread which initializes us.
+        if(pthread_getaffinity_np(pthread_self(),sizeof(mask),&mask) == 0){
+#else
+#error "Need cpu_set_t definition for this platform" // solaris is psetid_t
+#endif
+		for(cpu = 0 ; cpu < CPU_SETSIZE ; ++cpu){
+			if(CPU_ISSET(cpu,&mask)){
+				++count;
+			}
+		}
+	}
+        return count;
+}
 
 // This is the simple, O(N) latency pthread creation
 /* int blossom_pthreads(blossom_state *ctx,const pthread_attr_t *attr,
--- src/libblossom/schedule.h
+++ src/libblossom/schedule.h
@@ -5,42 +5,6 @@
 extern "C" {
 #endif
 
-#include <pthread.h>
-#if defined(__linux__)
-#include <sched.h>
-#elif defined(__freebsd__)
-#include <sys/param.h>
-#include <sys/cpuset.h>
-typedef cpuset_t cpu_set_t;
-#else
-#error "Need cpu_set_t definition for this platform" // solaris is psetid_t
-#endif
-
-// FreeBSD's cpuset.h (as of 7.2) doesn't provide CPU_COUNT, nor do older
-// Linux setups (including RHEL5). This requires only CPU_{SETSIZE,ISSET}.
-static inline unsigned
-portable_cpuset_count(void){
-        unsigned count = 0,cpu;
-	cpu_set_t mask;
-        
-#if defined(__freebsd__)
-        if(cpuset_getaffinity(CPU_LEVEL_CPUSET,CPU_WHICH_CPUSET,-1,
-                                sizeof(mask),&mask) == 0){
-#elif defined(__linux__)
-        // We might be only a subthread of a larger application; use the
-        // affinity mask of the thread which initializes us.
-        if(pthread_getaffinity_np(pthread_self(),sizeof(mask),&mask) == 0){
-#else
-#error "Need cpu_set_t definition for this platform" // solaris is psetid_t
-#endif
-		for(cpu = 0 ; cpu < CPU_SETSIZE ; ++cpu){
-			if(CPU_ISSET(cpu,&mask)){
-				++count;
-			}
-		}
-	}
-        return count;
-}       
 
 #ifdef __cplusplus
 }
