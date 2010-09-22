db307977d07f6e1e4f45c8cb53bc7a77846e05d3
Nick Black <dank@qemfd.net>
2010-09-21 22:23:01 -0400

    remove unused old code

--- src/libblossom/blossom.c
+++ src/libblossom/blossom.c
@@ -40,22 +40,6 @@ portable_cpuset_count(void){
 	return count;
 }
 
-// This is the simple, O(N) latency pthread creation
-/* int blossom_pthreads(blossom_state *ctx,const pthread_attr_t *attr,
-			void *(*fxn)(void *),void *arg){
-	unsigned z;
-
-	for(z = 0 ; z < ctx->tidcount ; ++z){
-		int ret;
-
-		if( (ret = pthread_create(&ctx->tids[z],attr,fxn,arg)) ){
-			// FIXME cleanup spawned threads...how?
-			return ret;
-		}
-	}
-	return 0;
-} */
-
 enum {
 	BLOOM_INITIALIZED,
 	BLOOM_SUCCESS,
